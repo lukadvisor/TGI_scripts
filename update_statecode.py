ccb4dbd8e7ebba3221db86d067dec530bfc69f07
lbrianza <lucabrianza@gmail.com>
2021-12-17 16:31:00 +0800

    add script to change statecode of inactive products

---
+++ update_statecode.py
@@ -0,0 +1,84 @@
+# !pip install adal , If Required
+import os
+import adal
+import requests
+import json
+import csv
+from dotenv import load_dotenv
+import datetime
+
+'''
+sources = ['aldinord','aldisued','aldiuk',
+            'amazon','amazonde','carrefour','discounto',
+            'euroelectronics','homedepot','johnlewis',
+            'leclerc','lidlde','lidles', 'lidlpl',
+            'mediamarkt','metro','otto',
+            'saturn','tchibode','walmart']
+'''
+
+sources = ['aldisued']
+
+# Global configs.
+load_dotenv(dotenv_path='.env')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+RESOURCE_URI = os.environ.get('RESOURCE_URI')
+AUTHORITY_URI = os.environ.get('AUTHORITY_URI')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+#After how many days to change statecode?
+LIMIT = 30
+
+# Get an access token.
+context = adal.AuthenticationContext(AUTHORITY_URI, api_version=None)
+token = context.acquire_token_with_client_credentials(RESOURCE_URI, CLIENT_ID, CLIENT_SECRET)
+#print(token)
+
+session = requests.Session()
+session.headers.update(dict(Authorization='Bearer {}'.format(token.get('accessToken'))))
+ 
+headers = {
+    'Authorization': 'Bearer ' + token.get('accessToken'),
+    'OData-MaxVersion': '4.0',
+    'OData-Version': '4.0',
+    'Content-Type': 'application/json',
+    'Prefer': 'return=representation'
+}
+
+#set these values to query your crm data
+
+crmwebapi = 'https://mysam-config.api.crm5.dynamics.com/api/data/v9.2/' #full path to web api endpoint
+crmwebapiquery_product = 'tk_customerproductmarketdatas'#?$select=tk_name #web api query (include leading /)
+crmwebapiquery_price = 'tk_customerproductmarketprices'#?$select=tk_features' #web api query (include leading /)
+crmwebapiquery = 'tk_customerproductmarketprices?$expand=tk_RelatedProduct'
+
+for source in sources:
+    print(f"Update statecode for {source} products...")
+    retailerquery = f"&$filter=contains(tk_RelatedProduct/tk_retailer,'{source}')"
+
+    # Request data.
+    r = session.get(crmwebapi+crmwebapiquery+retailerquery)
+    rawJson_str = r.content.decode('utf-8')
+    rawJson = json.loads(rawJson_str)
+    products = rawJson['value']
+
+    for product in products:
+        product_id = product['_tk_relatedproduct_value']
+        #statuscode = product['tk_RelatedProduct']['statuscode']
+        statecode = product['tk_RelatedProduct']['statecode']
+        modifiedon = product['tk_RelatedProduct']['modifiedon']
+        createdon = product['tk_RelatedProduct']['createdon']
+        last_date_str = modifiedon.split('T')[0]
+        first_date_str = createdon.split('T')[0]
+        last_date = datetime.datetime.strptime(last_date_str, '%Y-%m-%d')
+        first_date = datetime.datetime.strptime(first_date_str, '%Y-%m-%d')
+        difference = last_date - first_date
+        difference_in_days = difference.total_seconds()/86400
+        #print(statecode)
+        if(statecode==0):
+            if(difference_in_days>=LIMIT):
+                data = "{'value':1}"
+                patch_query = f'({product_id})/statecode'
+                r = session.put(crmwebapi+crmwebapiquery_product+patch_query, headers=headers, data=data)
+                print(f"Product {product_id} - Change statecode from 0 (active) to 1 (inactive)")
+
+
